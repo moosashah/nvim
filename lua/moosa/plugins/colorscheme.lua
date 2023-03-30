2cb1c16b98644af36cad532610518ae2370dde19
Moosa Shah <hello.moosashah@gmail.com>
2023-03-30 02:22:53 +0100

    remove italics as they dont work in alacritty

--- lua/moosa/plugins/colorscheme.lua
+++ lua/moosa/plugins/colorscheme.lua
@@ -1,12 +1,9 @@
 return {
-	'folke/tokyonight.nvim',
-	config = {
-		styles = {
-			functions = { italic = true },
-		},
-		sidebars = { 'qf', 'vista_kind', 'terminal', 'lazy' },
-		day_brightness = 0.3,
-		hide_inactive_statusline = true,
-		dim_inactive = true,
-	},
+  'folke/tokyonight.nvim',
+  config = {
+    sidebars = { 'qf', 'vista_kind', 'terminal', 'lazy' },
+    day_brightness = 0.3,
+    hide_inactive_statusline = true,
+    dim_inactive = true,
+  },
 }
