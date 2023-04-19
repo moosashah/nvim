b22d2f68cbdc64bc5089c62460859bc094a0c05d
Moosa Shah <hello.moosashah@gmail.com>
2023-04-19 17:06:55 +0100

    auto pairs

--- lua/moosa/plugins/treesitter.lua
+++ lua/moosa/plugins/treesitter.lua
@@ -1,25 +1,26 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  event = 'VimEnter',
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = 'all',
-      ignore_install = { 'phpdoc' },
-      rainbow = {
-        enable = true,
-        extended_mode = false,
-      },
-      highlight = {
-        enable = true,
-        disable = { 'css' },
-      },
-      indent = { enable = true, disable = { 'python', 'css' } },
-    })
-  end,
-  dependencies = {
-    { 'windwp/nvim-ts-autotag' },
-    { 'p00f/nvim-ts-rainbow' },
-    { 'lukas-reineke/indent-blankline.nvim'
+    'nvim-treesitter/nvim-treesitter',
+    event = 'VimEnter',
+    config = function()
+        require('nvim-treesitter.configs').setup({
+            ensure_installed = 'all',
+            ignore_install = { 'phpdoc' },
+            rainbow = {
+                enable = true,
+                extended_mode = false,
+            },
+            highlight = {
+                enable = true,
+                disable = { 'css' },
+            },
+            indent = { enable = true, disable = { 'python', 'css' } },
+            autotag = { enable = true }
+        })
+    end,
+    dependencies = {
+        { 'windwp/nvim-ts-autotag' },
+        { 'p00f/nvim-ts-rainbow' },
+        { 'lukas-reineke/indent-blankline.nvim'
+        },
     },
-  },
 }
