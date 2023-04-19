a8d926516709abd4da6bcdb98c3957327efc21bf
Moosa Shah <hello.moosashah@gmail.com>
2023-04-19 19:33:39 +0100

    unused option

--- lua/moosa/options.lua
+++ lua/moosa/options.lua
@@ -35,7 +35,6 @@ vim.opt.wrap = false       -- display lines as one long line
 vim.opt.scrolloff = 8      -- is one of my fav
 vim.opt.sidescrolloff = 8
 vim.opt.inccommand = 'split'
--- vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
 vim.opt.fillchars.eob = ' '
 vim.opt.shortmess:append 'c'
 vim.opt.whichwrap:append '<,>,[,],h,l'
