d76c347eb9de08c6f770fc82d88ae11b46135504
Moosa Shah <hello.moosashah@gmail.com>
2023-05-26 20:25:45 +0100

    change better quickfix keys

--- lua/moosa/plugins/better_quick_fix.lua
+++ lua/moosa/plugins/better_quick_fix.lua
@@ -2,9 +2,9 @@ return {
 	'kevinhwang91/nvim-bqf',
 	event = 'BufEnter',
 	keys = {
-		{ '<C-Down>', ':cnext<CR>' },
-		{ '<C-Up>', ':cprev<CR>' },
-		{ '<leader>qq', ':cclose<CR>' },
-		{ '<leader>qo', ':copen<CR>' },
+		{ 'µ', ':cnext<CR>', { silent = true } },
+		{ '≤', ':cprev<CR>', { silent = true } },
+		{ '∂', ':cclose<CR>', { silent = true } },
+		{ 'ƒ', ':copen<CR>', { silent = true } },
 	},
 }
