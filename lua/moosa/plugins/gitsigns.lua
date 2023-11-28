f615bcd5e3a8275a8a2aac05cc4b2202006963e4
Moosa Shah <hello.moosashah@gmail.com>
2023-11-28 22:22:23 +0000

    add keys to move between git changes

--- lua/moosa/plugins/gitsigns.lua
+++ lua/moosa/plugins/gitsigns.lua
@@ -21,4 +21,8 @@ return {
         numhl = true,
     },
     event = 'BufEnter',
+    keys = {
+        { ']', '<cmd>lua require("gitsigns").next_hunk()<CR>' },
+        { '[', '<cmd>lua require("gitsigns").prev_hunk()<CR>' },
+    },
 }
