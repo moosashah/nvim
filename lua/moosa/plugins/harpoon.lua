c7a8f306f394f280628b0a13d2eba83d4c00f824
Moosa Shah <hello.moosashah@gmail.com>
2023-12-31 22:12:50 +0000

    update nvim

--- lua/moosa/plugins/harpoon.lua
+++ lua/moosa/plugins/harpoon.lua
@@ -3,9 +3,9 @@ return {
     keys = {
         { '<leader>ma', '<cmd>lua require"harpoon.mark".add_file()<cr>' },
         { '<leader>mt', '<cmd>lua require"harpoon.ui".toggle_quick_menu()<cr>' },
-        { '∆', '<cmd>lua require("harpoon.ui").nav_file(1)<cr>' },
-        { '˚', '<cmd>lua require("harpoon.ui").nav_file(2)<cr>' },
-        { '¬', '<cmd>lua require("harpoon.ui").nav_file(3)<cr>' },
-        { '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>' },
+        { '<M-j>',      '<cmd>lua require("harpoon.ui").nav_file(1)<cr>' },
+        { '<M-k>',      '<cmd>lua require("harpoon.ui").nav_file(2)<cr>' },
+        { '<M-l>',      '<cmd>lua require("harpoon.ui").nav_file(3)<cr>' },
+        { '<C-f>',      '<cmd>silent !tmux neww tmux-sessionizer<CR>' },
     },
 }
