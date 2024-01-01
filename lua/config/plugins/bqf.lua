9d32c03b87f0213055ea2a5fcdd9c790f7403536
Moosa Shah <hello.moosashah@gmail.com>
2024-01-01 13:30:04 +0000

    remove hardcoded right alt key modified keymaps

--- lua/config/plugins/bqf.lua
+++ lua/config/plugins/bqf.lua
@@ -2,10 +2,9 @@ return {
     'kevinhwang91/nvim-bqf',
     event = 'BufEnter',
     keys = {
-        { '<M-m>', ':cnext<CR>', silent = true },
-        { '<M-,>', ':cprev<CR>', silent = true },
-        -- Using right alt sends the converted keys eg ƒ instead of <A-f> so need to hardcode expected key
-        { 'ƒ', ':cclose<CR>', silent = true },
-        { '∂', ':copen<CR>', silent = true },
+        { '<M-m>', ':cnext<CR>',  silent = true },
+        { '<M-,>', ':cprev<CR>',  silent = true },
+        { '<M-f>', ':cclose<CR>', silent = true },
+        { '<M-d>', ':copen<CR>',  silent = true },
     },
 }
