51eeea5d41f09f79c6c491f24b1ee9d82b7602a7
Moosa Shah <hello.moosashah@gmail.com>
2024-01-01 12:47:04 +0000

    cull options and formatting

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -2,10 +2,8 @@
 vim.api.nvim_create_autocmd({ 'user' }, {
     pattern = { 'AlphaReady' },
     callback = function()
-        vim.cmd [[
-     set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-     set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
-     ]]
+        --        vim.cmd("set showtabline=0 | autocmd BufUnload <buffer> set showtabline=0")
+        vim.cmd("set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3")
     end,
 })
 
--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -1,8 +1,7 @@
- return {
-     {
-         { 'nvim-lua/plenary.nvim' },
-         { 'nvim-tree/nvim-web-devicons' },
-         { 'christoomey/vim-tmux-navigator', event = 'VimEnter' },
-     },
- }
-
+return {
+    {
+        { 'nvim-lua/plenary.nvim' },
+        { 'nvim-tree/nvim-web-devicons' },
+        { 'christoomey/vim-tmux-navigator', event = 'VimEnter' },
+    },
+}
