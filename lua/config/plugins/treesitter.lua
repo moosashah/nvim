7df4eaf5ae9f3c00cc8c9c54ed34de2afbc19276
Moosa Shah <hello.moosashah@gmail.com>
2024-01-01 12:46:42 +0000

    autopairs and treesitter context

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,15 +1,21 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    dependencies = {
-        { 'windwp/nvim-ts-autotag' },
+    {
+
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        dependencies = {
+            { 'windwp/nvim-ts-autotag' },
+        },
+        config = function()
+            local cfg = require('nvim-treesitter.configs')
+            cfg.setup({
+                auto_install = true,
+                highlight = { enable = true },
+                autotag = { enable = true },
+            })
+        end
     },
-    config = function()
-        local cfg = require('nvim-treesitter.configs')
-        cfg.setup({
-            auto_install = true,
-            highlight = {enable = true},
-            autotag = {enable = true},
-        })
-    end
+    {
+        'nvim-treesitter/nvim-treesitter-context'
+    }
 }
