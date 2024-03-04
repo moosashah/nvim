493cbed3cca59fbcf4fc414c4eabc3bb7cbf5011
moosa shah <moosa.shah@medlify.com>
2024-03-04 18:32:41 +0000

    harpoon keybinding

--- lua/config/plugins/harpoon.lua
+++ lua/config/plugins/harpoon.lua
@@ -7,7 +7,7 @@ return {
         { '<M-j>',      '<cmd>lua require("harpoon.ui").nav_file(1)<cr>' },
         { '<M-k>',      '<cmd>lua require("harpoon.ui").nav_file(2)<cr>' },
         { '<M-l>',      '<cmd>lua require("harpoon.ui").nav_file(3)<cr>' },
-        { '<M-;>',      '<cmd>lua require("harpoon.ui").nav_file(4)<cr>' },
+        { '<M-h>',      '<cmd>lua require("harpoon.ui").nav_file(4)<cr>' },
         { '<C-f>',      '<cmd>silent !tmux neww tmux-sessionizer<CR>' },
     },
 }
