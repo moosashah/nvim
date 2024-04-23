7e10aa7ca757f743795cc430dc11b7ccc97bc4c6
Moosa Shah <hello.moosashah@gmail.com>
2024-04-23 20:07:41 +0100

    tokyonight brighter colors!!!!!

--- lua/config/plugins/tokyonight.lua
+++ lua/config/plugins/tokyonight.lua
@@ -2,7 +2,7 @@ return {
     'folke/tokyonight.nvim',
     priority = 1000,
     opts = {
-        day_brightness = 0.3,
+        day_brightness = 1,
     },
     config = function()
         vim.cmd.colorscheme('tokyonight-moon')
